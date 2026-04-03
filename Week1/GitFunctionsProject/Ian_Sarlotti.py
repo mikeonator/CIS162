1954edcf3d0ed3059001532d139aaae6795a48de
mikeonator <maudinyc@gmail.com>
2026-04-02 22:33:28 -0700

    Week 1 - We did it! I think...

---
+++ Week1/GitFunctionsProject/Ian_Sarlotti.py
@@ -0,0 +1,13 @@
+
+
+def rect_area(x, y):
+    area = x*y
+    return area
+
+# Request the dimension of a solid rectangular object
+# length = int(input("Enter the length of the the object as in integer: "))
+# width = int(input("Enter the width of the the object as in integer: "))
+
+# print(rect_area(length, width))
+
+# end
